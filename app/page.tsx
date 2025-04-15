7a7a5ae581cf40f851dafac2e77c05e286a297a5
Khoa Dinh <khoadinh2k@gmail.com>
2025-04-14 19:18:43 -0500

    feat: rough draft of supabase implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   SignInButton,
   SignUpButton,
@@ -6,7 +7,58 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 
+import { useEffect, useState } from "react";
+import { useSession, useUser } from "@clerk/nextjs";
+import { createClient } from "@supabase/supabase-js";
+
 export default function Home() {
+  const [tasks, setTasks] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [name, setName] = useState("");
+
+  const { user } = useUser();
+  const { session } = useSession();
+
+  function createClerkSupabaseClient() {
+    return createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_KEY!,
+      {
+        async accessToken() {
+          return session?.getToken() ?? null;
+        },
+      },
+    );
+  }
+
+  const client = createClerkSupabaseClient();
+
+  useEffect(() => {
+    if (!user) return;
+
+    async function loadTasks() {
+      setLoading(true);
+      const { data, error } = await client.from("tasks").select();
+      if (!error) setTasks(data);
+      setLoading(false);
+    }
+
+    loadTasks();
+  }, [user]);
+
+  async function createTask(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    await client.from("tasks").insert({
+      name,
+    });
+    window.location.reload();
+  }
+
+  async function deleteTask(task: number) {
+    await client.from("tasks").delete().eq("id", task);
+    window.location.reload();
+  }
+
   return (
     <div>
       <header className="flex h-16 items-center justify-end gap-4 p-4">
@@ -20,6 +72,40 @@ export default function Home() {
       </header>
       <div className="mx-auto flex w-full max-w-screen-md flex-col items-center justify-between gap-8 px-4 text-xl">
         <div> Hello World </div>
+
+        <h1>Tasks</h1>
+
+        {loading && <p>Loading...</p>}
+
+        {!loading &&
+          tasks.length > 0 &&
+          tasks.map((task: any) => (
+            <p key={task.id}>
+              {task.name}
+              <button
+                onClick={() => deleteTask(task.id)}
+                className={"ml-10 hover:cursor-pointer"}
+              >
+                ❌
+              </button>
+            </p>
+          ))}
+
+        {!loading && tasks.length === 0 && <p>No tasks found</p>}
+
+        <form onSubmit={createTask}>
+          <input
+            autoFocus
+            type="text"
+            name="name"
+            placeholder="Enter new task"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <button className={"hover:cursor-pointer"} type="submit">
+            Add
+          </button>
+        </form>
       </div>
     </div>
   );
